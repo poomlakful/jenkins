dbdd48b589aa674773cd6c578fb0f0bc299e73df
Poom Tammawutikul <p.tammawutikul@group-miki.com>
2020-12-23 18:26:33 +0700

    update version to 2.263.1

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM jenkins:2.60.3
 USER root
 RUN cd /usr/share/jenkins && \
         cp jenkins.war jenkins.war.old && \
-        wget http://updates.jenkins-ci.org/download/war/2.235.2/jenkins.war
+        wget https://updates.jenkins.io/download/war/2.263.1/jenkins.war
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker build -t local.docker.machine/jenkins:2.235.2 .
-docker run -d --name jenkins -p 28080:8080 -p 25000:50000 -v jenkins_data:/var/jenkins_home local.docker.machine/jenkins:2.235.2
+docker build -t local.docker.machine/jenkins:2.263.1 .
+docker run -d --name jenkins -p 28080:8080 -p 25000:50000 -v jenkins_data:/var/jenkins_home local.docker.machine/jenkins:2.263.1
